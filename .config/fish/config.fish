d268abf0a31545729b00f0271f061f78519cb975
raiguard <caleb.heuer@gmail.com>
2021-09-12 01:10:49 -0600

    Set NNN colors in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,15 @@
 fish_add_path /home/rai/.config/kak/bin
+
+set -x "blk" "04"
+set -x "chr" "04"
+set -x "dir" "04"
+set -x "exe" "00"
+set -x "reg" "00"
+set -x "hardlink" "00"
+set -x "symlink" "06"
+set -x "missing" "00"
+set -x "orphan" "01"
+set -x "fifo" "0F"
+set -x "sock" "0F"
+set -x "other" "02"
+set -x "NNN_FCOLORS" "$blk$chr$dir$exe$reg$hardlink$symlink$missing$orphan$fifo$sock$other"
