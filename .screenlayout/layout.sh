91de00a6ede48f60759bcda70280d834c13da556
raiguard <caleb.heuer@gmail.com>
2021-10-21 22:47:26 -0600

    Move screen layout to qtile autostart

--- .screenlayout/layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off --output USB-C-0 --off
