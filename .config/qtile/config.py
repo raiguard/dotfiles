5236692b8d19b3ad2a4d4b76037ab05982f13d85
raiguard <caleb.heuer@gmail.com>
2021-12-18 19:58:11 -0700

    Decrease bar font size, fix floating window border

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -186,8 +186,7 @@ layouts = [
 
 widget_defaults = dict(
     font='Source Code Pro Bold',
-    fontsize=13,
-    padding=3,
+    fontsize=12,
 )
 extension_defaults = widget_defaults.copy()
 focus_on_window_activation = 'focus'
@@ -340,16 +339,20 @@ dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-])
+floating_layout = layout.Floating(
+    border_focus=colors["blue"],
+    border_width=2,
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class='confirmreset'),  # gitk
+        Match(wm_class='makebranch'),  # gitk
+        Match(wm_class='maketag'),  # gitk
+        Match(wm_class='ssh-askpass'),  # ssh-askpass
+        Match(title='branchdialog'),  # gitk
+        Match(title='pinentry'),  # GPG key password entry
+    ]
+)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
