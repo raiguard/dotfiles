f279ad6bdd0be90e1a4d394a109f3e00cabd34ea
raiguard <caleb.heuer@gmail.com>
2022-01-10 17:32:19 -0700

    Wait for xrandr to finish, use redshift-gtk

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
+# We want xrandr to finish before qtile fully boots
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off --output USB-C-0 --off
+
+# Run these in the background
 picom & # Compositor
-/usr/bin/polkit-dumb-agent &	# Graphical authentication agent
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off --output USB-C-0 --off & # Screen layout
-redshift & # Screen dimming and night shift
+/usr/bin/polkit-dumb-agent & # Graphical authentication agent
+redshift-gtk & # Screen dimming and night shift
 ~/.nfancurve/temp.sh & # NVIDIA fan curves
 udiskie --tray & # Disk auto-mounting and tray icon
 nm-applet & # Network status in system tray
