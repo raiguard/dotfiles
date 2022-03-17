1efcba55c6217c4b067e6460db08db3c9c38478a
raiguard <caleb.heuer@gmail.com>
2022-03-16 18:29:03 -0600

    Misc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,7 +15,7 @@ alias lg lazygit
 alias fac1 /home/rai/dev/factorio/1/bin/x64/factorio
 alias fac2 /home/rai/dev/factorio/2/bin/x64/factorio
 alias facs-1.1 /home/rai/dev/projects/wube/Factorio-1.1/build/bin/Factorio_unity
-# alias facs-1.2 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64GCC/
+alias facs-1.2 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
 alias kbconfig "sh /home/rai/.config/keyboard.sh"
 alias stw "stow --dir ~/.dotfiles --target ~/"
 alias restart-kde "kquitapp5 plasmashell && kstart5 plasmashell"
