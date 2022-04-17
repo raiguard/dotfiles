ac23b08a17f3f9b0fcf5ab2e3e1216232323395a
raiguard <caleb.heuer@gmail.com>
2022-04-17 17:19:52 -0600

    Remove sway, call redshift at startup

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -8,3 +8,4 @@ udiskie --tray & # Disk auto-mounting and tray icon
 nm-applet & # Network status in system tray
 blueman-applet & # Bluetooth status in system tray
 pasystray & # Volume control in system tray
+redshift -o # Perform redshift exactly once (keeping it on introduces stutter)
