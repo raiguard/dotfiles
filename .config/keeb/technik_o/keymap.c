8f5434c7281a71dec7b9e80b0fb7306f16819456
raiguard <caleb.heuer@gmail.com>
2022-04-18 01:38:12 -0600

    Add game layer and use better trans keycode

--- .config/keeb/technik_o/keymap.c
+++ .config/keeb/technik_o/keymap.c
@@ -17,47 +17,59 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-  _MAIN,
+  _BASE,
   _NUM,
   _NAV,
+  _GAME,
   _MSE
 };
 
 #define LCTL_ESC MT(KC_LCTL, KC_ESC)
 #define NUM MO(_NUM)
 #define NAV MO(_NAV)
+#define GAME TG(_GAME)
 #define MSE TG(_MSE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_MAIN] = LAYOUT_ortho_4x12(
-    KC_TAB,   KC_Q,    KC_W,    KC_E, KC_R,    KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_ESC,
-    LCTL_ESC, KC_A,    KC_S,    KC_D, KC_F,    KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C, KC_V,    KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RETN,
-    _______,  _______, _______, MSE,  KC_LALT, KC_SPC, NUM,  NAV,  _______, _______, _______, _______
+  [_BASE] = LAYOUT_ortho_4x12(
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_ESC,
+    LCTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RETN,
+    _______,  _______, _______, KC_LGUI, KC_LALT, KC_SPC, NUM,  NAV,  MSE,     GAME,   _______, _______
   ),
 
+  // Numbers and symbols, optimized for the most common symbols across all programming languages
   [_NUM] = LAYOUT_ortho_4x12(
-    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_TRNS, KC_DLR,  KC_PLUS, KC_LPRN, KC_RPRN, KC_AT,   KC_PIPE, KC_MINS, KC_EQL,  KC_UNDS, KC_ASTR, _______,
-    KC_TRNS, KC_EXLM, KC_HASH, KC_LCBR, KC_RCBR, KC_TILD, KC_AMPR, KC_LBRC, KC_RBRC, KC_PERC, KC_ASTR, KC_TRNS,
-    _______, _______, _______, _______, KC_TRNS, KC_TRNS, _______, _______, _______, _______, _______, _______
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    _______, KC_DLR,  KC_PLUS, KC_LPRN, KC_RPRN, KC_AT,   KC_PIPE, KC_MINS, KC_EQL,  KC_UNDS, KC_ASTR, _______,
+    _______, KC_EXLM, KC_HASH, KC_LCBR, KC_RCBR, KC_TILD, KC_AMPR, KC_LBRC, KC_RBRC, KC_PERC, KC_CIRC, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
+  // Function and arrow keys, plus media controls
   [_NAV] = LAYOUT_ortho_4x12(
-    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,  KC_MPLY, KC_PSCR,
-    KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-    KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______, _______,
-    _______, _______, _______, _______, KC_TRNS, KC_TRNS, _______, _______, _______, _______,  _______, _______
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,  KC_MPLY, KC_PSCR,
+    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______, RESET,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______
   ),
 
-  // Mouse layer is toggled instead of held, and passes through all unused keys
-  // This allows it to be used while gaming
+  // Shift thumb bar over by one for use with WASD
+  [_GAME] = LAYOUT_ortho_4x12(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, KC_LGUI, KC_LALT, KC_SPC,  NUM,     _______, _______, _______, _______, _______, _______
+  ),
+
+  // Mouse controls
+  // Also have a way to access NAV in case we're gaming
   [_MSE] = LAYOUT_ortho_4x12(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_D, KC_MS_U, KC_WH_U, KC_ACL2, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, KC_WH_D, KC_MS_U, KC_WH_U, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, _______,
+    _______, _______, _______, _______, _______, _______, NAV,     KC_BTN3, _______, _______, KC_ACL2, _______,
+    _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______
   ),
 
 };
