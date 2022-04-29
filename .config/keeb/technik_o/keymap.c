5e2e860ce897a689930808fba14ba5419c312535
raiguard <caleb.heuer@gmail.com>
2022-04-29 17:07:08 -0600

    Fix control, return, and backspace not working

--- .config/keeb/technik_o/keymap.c
+++ .config/keeb/technik_o/keymap.c
@@ -24,7 +24,7 @@ enum layers {
   _MSE
 };
 
-#define LCTL_ESC MT(KC_LCTL, KC_ESC)
+#define LCTL_ESC MT(MOD_LCTL, KC_ESC)
 #define NUM MO(_NUM)
 #define NAV MO(_NAV)
 #define GAME TG(_GAME)
@@ -33,9 +33,9 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_ortho_4x12(
-    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_ESC,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
     LCTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RETN,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
     _______,  _______, _______, KC_LGUI, KC_LALT, KC_SPC, NUM,  NAV,  MSE,     GAME,   _______, _______
   ),
 
