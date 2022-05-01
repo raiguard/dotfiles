5ed1c21064e63f9bf3e9b09ac8d519fe2f94bcba
raiguard <caleb.heuer@gmail.com>
2022-04-30 21:22:54 -0600

    Make Sway aware of environment variables, clean up bash and fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,3 @@
-fish_add_path "/home/rai/.local/bin"
-fish_add_path "/home/rai/.cargo/bin"
-fish_add_path "/home/rai/bin"
-
-set -x KKS_USE_GITDIR_SESSIONS 1
-
-set -x EDITOR "kks edit"
-set -x PAGER "less"
-set -x SHELL "fish"
-set -x KAKOUNE_POSIX_SHELL "/bin/dash"
 set fish_greeting
 
 alias k "kks edit"
@@ -17,8 +7,5 @@ alias fac1 /home/rai/dev/factorio/1/bin/x64/factorio
 alias fac2 /home/rai/dev/factorio/2/bin/x64/factorio
 alias facs-1.1 /home/rai/dev/projects/wube/Factorio-1.1/build/bin/Factorio_unity
 alias facs-1.2 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
-alias kbconfig "sh /home/rai/.config/keyboard.sh"
-alias stw "stow --dir ~/.dotfiles --target ~/"
-alias restart-kde "kquitapp5 plasmashell && kstart5 plasmashell"
 
 source /usr/share/autojump/autojump.fish
--- .profile
+++ .profile
@@ -1,5 +1,16 @@
-# Keyboard config
-source "$HOME/.config/keyboard.sh"
-
-# Because?
 export EDITOR="kks edit"
+export PAGER="kks edit"
+export VISUAL="kks edit"
+export KKS_USE_GITDIR_SESSIONS=1
+export KAKOUNE_POSIX_SHELL=/bin/dash
+
+export PATH="$HOME/bin:$HOME/.cargo/bin:$PATH"
+
+alias k="kks edit"
+alias dlg="lazygit --work-tree ~/.dotfiles"
+alias lg=lazygit
+alias fac1=/home/rai/dev/factorio/1/bin/x64/factorio
+alias fac2=/home/rai/dev/factorio/2/bin/x64/factorio
+alias facs-1.1=/home/rai/dev/projects/wube/Factorio-1.1/build/bin/Factorio_unity
+alias facs-1.2=/home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
+
