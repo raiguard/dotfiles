1447d4237fcfa5780cbb6cbbe2f73cc724c2538d
raiguard <caleb.heuer@gmail.com>
2022-05-07 13:11:02 -0600

    Remove homerow mods - tapping term conflicted with the base layer

--- .config/keeb/technik_o/keymap.c
+++ .config/keeb/technik_o/keymap.c
@@ -18,7 +18,6 @@
 
 enum layers {
   _BASE,
-  _HMMODS,
   _GAME,
   _MSE,
   _NUM,
@@ -26,7 +25,6 @@ enum layers {
 };
 
 #define LCTL_ESC MT(MOD_LCTL, KC_ESC)
-#define HMMODS TG(_HMMODS)
 #define NUM MO(_NUM)
 #define MSE TG(_MSE)
 #define GAME TG(_GAME)
@@ -55,14 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    _______,  _______, _______, KC_LGUI, KC_LALT, KC_SPC, NUM,  NAV,  _______, _______, _______, HMMODS
-  ),
-
-  [_HMMODS] = LAYOUT_ortho_4x12(
-    XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y, KC_U,   KC_I,    KC_O,    KC_P,      KC_BSPC,
-    KC_ESC,  LGUI_A,  LALT_S,  LCTL_D,  LSFT_F, KC_G,   KC_H, RSFT_J, RCTL_K,  RALT_L,  RGUI_SCLN, KC_QUOT,
-    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT,
-    _______, _______, _______, _______, KC_TAB, KC_SPC, NUM,  NAV,    _______, _______, _______,   _______
+    _______,  _______, _______, KC_LGUI, KC_LALT, KC_SPC, NUM,  NAV,  _______, _______, _______, _______
   ),
 
   // Shift thumb bar over by one for use with WASD
