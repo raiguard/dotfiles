7f2b5635c89d9c332674cf46bf078396152a1a61
Caleb Heuer <ch@raiguard.me>
2022-07-17 13:56:25 -0600

    Adjust factorio 1.1.x launch path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ alias dlg "lazygit --work-tree ~/.dotfiles"
 alias lg lazygit
 alias fac1 /home/rai/dev/factorio/1/bin/x64/factorio
 alias fac2 /home/rai/dev/factorio/2/bin/x64/factorio
-alias facs-1.1 /home/rai/dev/projects/wube/Factorio-1.1/build/bin/Debugx64Clang/factorio-run
+alias facs-1.1 /home/rai/dev/projects/wube/Factorio-1.1/bin/Debugx64Clang/factorio-run
 alias facs-1.2 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
 
 source /usr/share/autojump/autojump.fish
