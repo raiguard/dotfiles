ddfdabed539266a0c8c066417371cfe7de0d4540
Caleb Heuer <ch@raiguard.me>
2022-09-16 19:53:46 -0600

    Misc factorio-related stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ alias lg lazygit
 alias t "todoist-cli --color"
 alias fac1 /home/rai/dev/factorio/1/bin/x64/factorio
 alias fac2 /home/rai/dev/factorio/2/bin/x64/factorio
+alias fac3 /home/rai/dev/factorio/3/bin/x64/factorio
 alias facs-1.1 /home/rai/dev/projects/wube/Factorio-1.1/bin/Debugx64Clang/factorio-run
 alias facs-1.2 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
 
