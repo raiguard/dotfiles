006451fea1b1bf4591d5aaac4b6cd08179b03769
Caleb Heuer <ch@raiguard.me>
2022-09-18 03:28:39 -0600

    Update non-split technik layout

--- .config/keeb/technik_o/keymap.c
+++ .config/keeb/technik_o/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Boardsource
+  /* Copyright 2020 Boardsource
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,86 +18,36 @@
 
 enum layers {
   _BASE,
-  _BASE_HMMODS,
-  _GAME,
-  _MSE,
   _NUM,
   _SYM,
   _NAV
 };
 
 #define LCTL_ESC MT(MOD_LCTL, KC_ESC)
-#define NUM_SPC LT(_NUM, KC_SPC)
+#define SYM_ENT LT(SYM, KC_ENT)
+#define NUM_SPC LT(NUM, KC_SPC)
 #define NUM MO(_NUM)
 #define SYM MO(_SYM)
-#define SYM_ENT LT(_SYM, KC_ENT)
 #define MSE TG(_MSE)
-#define GAME TG(_GAME)
 #define NAV MO(_NAV)
-#define NAV_BSPC LT(_NAV, KC_BSPC)
-
-#define BASE_HMMODS TG(_BASE_HMMODS)
-
-#define LGUI_A MT(MOD_LGUI, KC_A)
-#define LALT_S MT(MOD_LALT, KC_S)
-#define LCTL_D MT(MOD_LCTL, KC_D)
-#define LSFT_F MT(MOD_LSFT, KC_F)
-
-#define RSFT_J MT(MOD_LSFT, KC_J)
-#define RCTL_K MT(MOD_LCTL, KC_K)
-#define RALT_L MT(MOD_LALT, KC_L)
-#define RGUI_QUOT MT(MOD_LGUI, KC_QUOT)
-
-const uint16_t PROGMEM tab[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM toggle_mouse[] = {NUM_SPC, SYM, COMBO_END};
-const uint16_t PROGMEM toggle_game[] = {KC_LALT, NAV, COMBO_END};
-combo_t key_combos[COMBO_COUNT] = {
-    COMBO(tab, KC_TAB),
-    COMBO(toggle_mouse, MSE),
-    COMBO(toggle_game, GAME)
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+  // Two dedicated space keys, one for regular use and another for gaming on WASD
   [_BASE] = LAYOUT_ortho_4x12(
-    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
-    LCTL_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    BASE_HMMODS, _______, _______, KC_LGUI, KC_LALT, NUM_SPC, SYM,  NAV,  _______, _______, _______, _______
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
+    LCTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    _______,  _______, _______, KC_LALT, KC_SPC, NUM_SPC, SYM_ENT, NAV,  KC_LGUI, _______, _______, _______
   ),
 
-  [_BASE_HMMODS] = LAYOUT_ortho_4x12(
-    XXXXXXX,     KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,      XXXXXXX,
-    XXXXXXX,     LGUI_A,  LALT_S,  LCTL_D,  LSFT_F, KC_G,    KC_H,    RSFT_J,   RCTL_K,  RALT_L,  RGUI_QUOT, XXXXXXX,
-    XXXXXXX,     KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,   XXXXXXX,
-    BASE_HMMODS, XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC, NUM_SPC, SYM_ENT, NAV_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX
-  ),
-
-  // Shift thumb bar over by one for use with WASD, and free up spacebar
-  [_GAME] = LAYOUT_ortho_4x12(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, KC_LGUI, KC_LALT, KC_SPC,  NUM,     _______, _______, _______, _______, _______, _______
-  ),
-
-  // Mouse controls
-  [_MSE] = LAYOUT_ortho_4x12(
-    _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ACL2, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_BTN3, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______
-  ),
-
-  // Numpad and F keys
   [_NUM] = LAYOUT_ortho_4x12(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_MINS, KC_7, KC_8,    KC_9,    KC_ASTR, _______,
-    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_PLUS, KC_4, KC_5,    KC_6,    KC_COMM, _______,
-    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_SLSH, KC_1, KC_2,    KC_3,    KC_DOT,  _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_0, _______, _______, _______, _______
+    _______, _______, KC_7,    KC_8,    KC_9,    KC_MINS, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
+    _______, _______, KC_4,    KC_5,    KC_6,    KC_0,    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
+    _______, _______, KC_1,    KC_2,    KC_3,    KC_DOT,  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  // Symbols
   [_SYM] = LAYOUT_ortho_4x12(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_QUES, _______,
     _______, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, _______,
@@ -106,10 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  // Arrow keys and media controls
   [_NAV] = LAYOUT_ortho_4x12(
-    RGB_TOG, KC_INS,  _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,  KC_MPLY, KC_PSCR,
-    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, KC_DEL,
+    _______, KC_PSCR, KC_VOLD, KC_MUTE, KC_VOLU, KC_BRIU, _______, _______, _______, _______,  _______, KC_DEL,
+    _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, KC_INS,
     _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, RESET
   )
