3a22e48d43a64b66bdf3d7b13cc621fd22bd4ed2
Caleb Heuer <ch@raiguard.me>
2022-09-20 22:56:32 -0600

    More convenient aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,10 +10,10 @@ alias f1 /home/rai/dev/factorio/1/bin/x64/factorio
 alias f2 /home/rai/dev/factorio/2/bin/x64/factorio
 alias f3 /home/rai/dev/factorio/3/bin/x64/factorio
 alias f4 /home/rai/dev/factorio/4/bin/x64/factorio
-alias fd1.1 /home/rai/dev/projects/wube/Factorio-1.1/bin/Debugx64Clang/factorio-run
-alias fr1.1 /home/rai/dev/projects/wube/Factorio-1.1/bin/Releasex64Clang/factorio-run
-alias fd1.2 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
-alias fr1.2 /home/rai/dev/projects/wube/Factorio-1.1/bin/Releasex64Clang/factorio-run
+alias fd11 /home/rai/dev/projects/wube/Factorio-1.1/bin/Debugx64Clang/factorio-run
+alias fr11 /home/rai/dev/projects/wube/Factorio-1.1/bin/Releasex64Clang/factorio-run
+alias fd12 /home/rai/dev/projects/wube/Factorio-1.2/bin/Debugx64Clang/factorio-run
+alias fr12 /home/rai/dev/projects/wube/Factorio-1.1/bin/Releasex64Clang/factorio-run
 
 source /usr/share/autojump/autojump.fish
 
