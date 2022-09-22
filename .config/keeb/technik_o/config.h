8ac283fbf92b4fda41b3833ec8598a780b756e52
Caleb Heuer <ch@raiguard.me>
2022-09-21 21:13:00 -0600

    More keymap updates...

--- .config/keeb/technik_o/config.h
+++ .config/keeb/technik_o/config.h
@@ -1,1 +1,5 @@
-#pragma once
+// Allow for quicker modifier usage
+#define PERMISSIVE_HOLD
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
--- .config/keeb/technik_o/keymap.c
+++ .config/keeb/technik_o/keymap.c
@@ -38,24 +38,24 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_ortho_4x12(
-      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,       KC_Y,         KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-      CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,       KC_H,         KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,       KC_N,         KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-      _______,  KC_LGUI, _______, KC_LALT, KC_SPC, NUMBER_SPC, SYMBOL_ENTER, NAVIGATE, KC_RALT, _______, _______, _______
+      KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,       KC_Y,         KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+      CTRL_ESC, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,       KC_H,         KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,       KC_N,         KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+      _______,  KC_LGUI, _______, KC_SPC, KC_LALT, NUMBER_SPC, SYMBOL_ENTER, NAVIGATE, KC_RALT, KC_RGUI, _______, _______
   ),
 
   [_NUMBER] = LAYOUT_ortho_4x12(
-      _______, KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-      _______, KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      _______, KC_MINUS, KC_EQUAL, KC_GRAVE, KC_BSLS, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______,  _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+      _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_SYMBOL] = LAYOUT_ortho_4x12(
-      _______, KC_F11,  KC_F12,  KC_F13,   KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
-      _______, S(KC_1), S(KC_2), S(KC_3),  S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______,
-      _______, KC_UNDS, KC_PLUS, KC_TILDE, KC_PIPE, _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-      _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_QUES, _______,
+      _______, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, _______,
+      _______, KC_SLSH, KC_GRV,  KC_DQUO, KC_QUOT, KC_UNDS, KC_TILD, KC_PIPE, KC_SCLN, KC_COLN, KC_BSLS, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_NAVIGATE] = LAYOUT_ortho_4x12(
