6354283b0ef3e9ac1a9e0b7b50521838c5d1f312
Caleb Heuer <ch@raiguard.me>
2022-11-06 01:18:42 -0700

    Show LAN IP

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -4,13 +4,19 @@ darkred="#be5046"
 green="#98c379"
 lightorange="#e5c07b"
 # darkorange="#d19a66"
-# blue="#61afef"
+blue="#61afef"
 # magenta="#c678dd"
 # cyan="#56b6c2"
 
 spacer=" <span foreground='$comment'>|</span> "
 
 # TODO: Cache wttr.in and display it over a day
+
+# IP address
+ip_address=$(ip address | grep "wlan0" | grep "inet" | awk '{ print $2 }')
+if [ -n "$ip_address" ]; then
+    ip_address="<span foreground='$blue'>$ip_address</span>  "
+fi
 
 # Battery info
 if [ "$HOSTNAME" = "uraya" ]; then
@@ -22,4 +28,4 @@ if [ "$HOSTNAME" = "uraya" ]; then
     bat="<span foreground='$bat_color'>$charge%</span>  "
 fi
 date=$(date +'%a %b %d %-H:%M:%S %Z')
-echo "$bat$date"
+echo "$bat$ip_address$date"
