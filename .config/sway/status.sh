2733e4038c4666cb010c192b5574b00b3ee461e2
Caleb Heuer <ch@raiguard.me>
2022-11-09 12:05:23 -0700

    Display SSID instead of IP

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,5 +1,5 @@
-# lightred="#e06c75"
-darkred="#be5046"
+lightred="#e06c75"
+# darkred="#be5046"
 green="#98c379"
 # lightorange="#e5c07b"
 # darkorange="#d19a66"
@@ -7,12 +7,13 @@ blue="#61afef"
 # magenta="#c678dd"
 # cyan="#56b6c2"
 
-# TODO: Cache wttr.in and display it over a day
+# TODO: Cache weather data and display it over a day
 
-# IP address
-ip_address=$(ip -br address | grep UP | head -1 | awk '{ print $1 " " $3 }' | sed "s/\\/.*//")
-if [ -n "$ip_address" ]; then
-    ip_address="<span foreground='$blue'>$ip_address</span>  "
+# Network
+# FIXME: This only works for wireless connections
+ssid=$(iwconfig | grep ESSID | awk -F '"' '{ print $2 }')
+if [ -n "$ssid" ]; then
+    network="<span foreground='$blue'>$ssid</span>  "
 fi
 
 # Battery info
@@ -20,9 +21,9 @@ if [ "$HOSTNAME" = "uraya" ]; then
     bat_color=$green
     charge=$(cat /sys/class/power_supply/BAT0/capacity)
     if [ "$charge" -lt 21 ]; then
-        bat_color=$darkred
+        bat_color=$lightred
     fi
     bat="<span foreground='$bat_color'>$charge%</span>  "
 fi
 date=$(date +'%a %b %d %-H:%M:%S %Z')
-echo "$bat$ip_address$date"
+echo "$bat$network$date"
