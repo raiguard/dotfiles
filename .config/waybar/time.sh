208a109066f4710383bb0cb0ad501fed8b7bf443
Caleb Heuer <ch@raiguard.me>
2024-05-19 18:35:03 -0600

    Switch from swaybar to waybar, other misc

---
+++ .config/waybar/time.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+date=$(date +'%a %b %d')
+if [ $(date +%Z) != "CEST" ]; then
+  prague="$(TZ=Europe/Prague date +'%-H:%M %Z') "
+fi
+local=$(date +"%-H:%M %Z")
+echo "$prague$local"
