80db3736e2b0c6e10c0b7959328d8252437fd3b5
Caleb Heuer <ch@raiguard.me>
2024-07-29 01:58:55 -0600

    Fix quoting issues in nnn picker script

--- .config/kak/nnn-pick.sh
+++ .config/kak/nnn-pick.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # Invokes NNN to pick a file and opens the chosen file in kakoune.
 
-start_location=$1 kak_client=$2 kak_session=$3
+start_location="$1" kak_client="$2" kak_session="$3"
 if ! [ -f "$start_location" ]; then
   start_location="$(dirname '$start_location')"
 fi
 
 tmp="${TMPDIR:-/tmp}/nnn_pick_$$"
 nnn -p "$tmp" "$start_location"
-filename=$(cat "$tmp")
+filename=$(cat "$tmp" | sed "s/'/\\'/g")
 rm "$tmp"
 
 if [ -n "$filename" ]; then
-  echo "evaluate-commands -client $kak_client 'edit \"$filename\"'" | kak -p $kak_session
+  echo "evaluate-commands -client '$kak_client' %{edit \"$filename\"}" | kak -p "$kak_session"
 fi
