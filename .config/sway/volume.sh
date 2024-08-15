cc5f8cd3aa543881017af1fa6ba4921a4b3478da
Caleb Heuer <ch@raiguard.me>
2024-08-15 01:47:32 -0600

    Fonts and other tweaks

--- .config/sway/volume.sh
+++ .config/sway/volume.sh
@@ -3,4 +3,5 @@
 pamixer -$1 5
 vol=$(pamixer --get-volume)
 voldisp=$(pamixer --get-volume-human)
+mpv /usr/share/sounds/gnome/default/alerts/click.ogg & disown
 notify-send -u low -h int:value:$vol -h string:x-dunst-stack-tag:volume -t 1500 -c system "volume - $voldisp"
